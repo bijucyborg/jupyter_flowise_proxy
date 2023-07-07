f849b462183640a028786c0241a406e1700c9ff7
Biju Krishnan <biju.krishnan@gmail.com>
2023-07-07 16:59:38 +0200

    Modified setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 import setuptools
 
+# read the contents of your README file
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+
 setuptools.setup(
     name="jupyter-h2o-llm-studio-proxy",
     version='1.0dev',
-    url="https://github.com/jupyterhub/jupyter-server-proxy/tree/HEAD/contrib/h2o-llm-studio",
+    url="https://github.com/bijucyborg/jupyter_h2o-llm-studio_proxy",
     author="biju.krishnan@datasiens.ai",
     description="biju.krishnan@datasiens.ai",
     packages=setuptools.find_packages(),
 	keywords=['Jupyter'],
 	classifiers=['Framework :: Jupyter'],
     install_requires=[
-        'jupyter-server-proxy'
+        'jupyter-server-proxy>=1.5.0'
     ],
     entry_points={
         'jupyter_serverproxy_servers': [
