864422d0a97d25550a993b1bca8342ae4a287464
Biju Krishnan <biju.krishnan@gmail.com>
2023-07-07 17:10:42 +0200

    Modified setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import setuptools
 from os import path
 
+
 # read the contents of your README file
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
@@ -9,22 +10,24 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setuptools.setup(
     name="jupyter-h2o-llm-studio-proxy",
-    version='1.0dev',
+    version="0.2.0",
     url="https://github.com/bijucyborg/jupyter_h2o-llm-studio_proxy",
-    author="biju.krishnan@datasiens.ai",
-    description="biju.krishnan@datasiens.ai",
+    author="DataSiens",
+    description="info@datasiens.ai",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
-	keywords=['Jupyter'],
-	classifiers=['Framework :: Jupyter'],
+    keywords=["jupyter", "h2o-llm-studio", "jupyterhub", "jupyter-server-proxy"],
+    classifiers=["Framework :: Jupyter"],
     install_requires=[
-        'jupyter-server-proxy>=1.5.0'
+        "jupyter-server-proxy>=1.5.0",
     ],
     entry_points={
-        'jupyter_serverproxy_servers': [
-            'h2ollmstudio = jupyter_h2o-llm-studio_proxy:setup_h2ollmstudio',
+        "jupyter_serverproxy_servers": [
+            "h2ollmstudio = jupyter_h2o-llm-studio_proxy:setup_h2ollmstudio",
         ]
     },
     package_data={
-        'jupyter_h2o-llm-studio_proxy': ['icons/*'],
+        "jupyter_pgweb_proxy": ["icons/logo.svg"],
     },
 )
